6ae03ffdb29fe07c6aa81bb3f0fbd06242d153e0
4firerain4 <4firerain4@gmail.com>
2024-11-15 10:01:04 +0300

    Реализована основная логика программы

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using System.Text.RegularExpressions;
-using Yu_Gi_Oh.CardTranslator.DAL;
+﻿using Yu_Gi_Oh.CardTranslator.DAL;
 using Yu_Gi_Oh.CardTranslator.Model;
 using Yu_Gi_Oh.CardTranslator.Services;
 
@@ -8,7 +6,38 @@ namespace Yu_Gi_Oh.CardTranslator;
 
 static class Program
 {
+    public static readonly string CacheDir = AppDomain.CurrentDomain.BaseDirectory + "Cache";
     static async Task Main(string[] args)
+    {
+        List<Card> cards;
+
+        if (!File.Exists(CacheDir + "/data.csv"))
+        {
+            await GetCardsData();
+            cards = await DataPorcessor.ReadDataAsync();
+        }
+        else
+        {
+            //await DataTranslator.Translate();
+            cards = await DataPorcessor.ReadDataAsync();
+        }
+
+        foreach (var card in cards)
+        {
+            CardCleaner.Clean(card);
+        }
+
+        Console.ReadKey();
+    }
+
+    static List<string> GetNames()
+    {
+        var rawCardsNames = File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "cards.txt").ToList();
+        var CardsNames = rawCardsNames.Select(x => x.Trim()).ToList();
+        return CardsNames;
+    }
+
+    private static async Task GetCardsData()
     {
         int maxDegreeOfParallelism = 8;
         SemaphoreSlim semaphore = new(maxDegreeOfParallelism);
@@ -35,14 +64,6 @@ static class Program
 
         await Task.WhenAll(tasks);
 
-        Console.WriteLine("ГатовА");
-        Console.ReadKey();
-    }
-
-    static List<string> GetNames()
-    {
-        var rawCardsNames = File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + "cards.txt").ToList();
-        var CardsNames = rawCardsNames.Select(x => x.Trim()).ToList();
-        return CardsNames;
+        DataPorcessor.WriteData(cards);
     }
 }
